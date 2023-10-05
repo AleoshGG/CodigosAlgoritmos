47db1bea0deec2979481d0ae7c7b40d3376150c6
Alexis Guzman <spartanthelexvro@gmail.com>
2023-10-05 09:48:46 -0600

    Ejercicios de funciones

--- codigo05.cpp
+++ codigo05.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+void validarMes(string nombreMes){
+    if (nombreMes=="Marzo")
+    {
+        cout<<"\nCorrecto";
+    }else{
+        cout<<"\nIncorrecto";
+    }
+}
+
 void validarMatricula(int matricula){
     while (matricula<100000 || matricula>999999)
     {
@@ -13,15 +22,18 @@ void validarMatricula(int matricula){
     cout<<"\nDatos correctos";
 
 }
-
+//paso de parametro por valor 
 void solicitarDatos(){
     int matricula;
-    string nombre;
+    string nombre, mes;
     cout<<"\nIngresa tu matricula: ";
     cin>>matricula;
     cout<<"\nIngresa tu primer nombre: ";
     cin>>nombre;
     validarMatricula(matricula);
+    cout<<"\nIngresa tu mes de nacimiento: ";
+    cin>>mes;
+    validarMes(mes);
 }
 
 main(){
