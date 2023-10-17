097af9fd7e0cf7794895637a7ab7c1b480cc9a79
Alexis Guzman <spartanthelexvro@gmail.com>
2023-10-17 09:43:15 -0600

    Uso de la libreria cmath

--- codigo06.cpp
+++ codigo06.cpp
@@ -1,15 +1,54 @@
-//Exponentes
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    int base=5;
-    int exponente=5;
-    int resultado= pow(base,exponente);
+void potencia(){
+    int base,exponente,resultado;
+    cout<<"Bienvenido a Potencia\n";
+    cout<<"Dame el numero base: ";
+    cin>>base;
+    cout<<"Dame el numero de la potencia: ";
+    cin>>exponente;
+    resultado=pow(base,exponente);
+    cout<<"El resultado es: "<<resultado;
+}
 
-    cout<<resultado;
+void raiz(){
+    int numero,resultado;
+    cout<<"Bienvenido a Raices\n";
+    cout<<"Dame el numero: ";
+    cin>>numero;
+    if(numero<=0){
+        cout<<"\n-------No se puede-------";
+    }
+    resultado=sqrt(numero);
+    cout<<"El resultado es: "<<resultado;
+}
+
+void menu(){
+    int opcion;
+    cout<<"Menu de opciones\n";
+    cout<<"1. Exponete\n";
+    cout<<"2. Raiz\n";
+    cin>>opcion;
+
+    switch (opcion)
+    {
+    case 1:
+        potencia();
+        break;
+    case 2: 
+        raiz();
+        break;
+    default:cout<<"Error";
+        break;
+    }
+}
+
+int main(int argc, char const *argv[])
+{   
+    menu();
     return 0;
 }
