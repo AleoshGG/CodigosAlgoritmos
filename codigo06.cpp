238c82ac6aded9bf623e013681bcad789d7a865d
Alexis Guzman <spartanthelexvro@gmail.com>
2023-10-17 09:12:57 -0600

    Uso de la libreria cmath

---
+++ codigo06.cpp
@@ -0,0 +1,15 @@
+//Exponentes
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int base=5;
+    int exponente=5;
+    int resultado= pow(base,exponente);
+
+    cout<<resultado;
+    return 0;
+}
